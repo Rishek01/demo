9fccbff232769e870edbc40d5d04ce2175d39f89
Rishek Kumar <163520579+Rishek01@users.noreply.github.com>
2024-11-18 12:08:33 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("click-me");
+    button.addEventListener("click", function() {
+        alert("You clicked the button!");
+    });
+});
