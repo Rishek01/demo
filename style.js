372c6158b97eceb99e87a0192517f85173977b9f
Rishek Kumar <163520579+Rishek01@users.noreply.github.com>
2024-11-18 12:15:28 +0530

    Delete style.js

--- style.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("click-me");
-    button.addEventListener("click", function() {
-        alert("You clicked the button!");
-    });
-});
